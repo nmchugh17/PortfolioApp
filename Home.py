f445f6f4d4c5f18505e9950693a5ebda8913490b
nmchugh17 <nmchu17@gmail.com>
2024-06-30 21:10:30 +0100

    Update to show projects if there are valid links

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas
+import pandas as pd
 
 # Sets the layout on the streamlit app to wide
 st.set_page_config(layout="wide")
@@ -33,17 +33,19 @@ st.write(content2)
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 # Use pandas do import csv file data into a dataframe
-df = pandas.read_csv("data.csv", sep=";")
+df = pd.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
+        if not pd.isnull(row['url']):
+            st.header(row["title"])
+            st.write(row["description"])
+            st.image("images/" + row["image"])
+            st.write(f"[Source Code]({row['url']})")
 with col4:
     for index, row in df[10:].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
+        if not pd.isnull(row['url']):
+            st.header(row["title"])
+            st.write(row["description"])
+            st.image("images/" + row["image"])
+            st.write(f"[Source Code]({row['url']})")
