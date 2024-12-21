6077920e31b6ba8217485303545ce4b39f9154e3
nmchugh17 <nmchu17@gmail.com>
2024-12-21 20:55:02 +0000

    Contact file: robot checkbox and exception handling added

--- pages/Contact.py
+++ pages/Contact.py
@@ -6,7 +6,8 @@ st.header("Contact Me")
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
     raw_message = st.text_area("Your message here")
-    message = f"""\
+    captcha = st.checkbox("I am not a robot")
+    message = f"""\ 
 Subject: New email from {user_email}
 
 From: {user_email}
@@ -14,5 +15,11 @@ From: {user_email}
 """
     button = st.form_submit_button("Submit")
     if button:
-        send_email(message)
-        st.info("Your email was sent successfully.")
+        if captcha:
+            try:
+                send_email(message)
+                st.info("Your email was sent successfully.")
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+        else:
+            st.warning("Please confirm that you are not a robot.")
